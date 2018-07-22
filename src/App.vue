a64a184e96d1e117484fc96857669d247cd40b6a
Daniel Charvát <charvat.dan@gmail.com>
2018-07-22 12:48:07 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'vue-codepen',
+}
+</script>
+
+<style>
+</style>
