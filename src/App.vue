c19fb7bd7c2aadd93c3935bd468b47b10b15edef
Daniel Charvát <charvat.dan@gmail.com>
2018-07-22 12:59:00 +0200

    Add working example

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,51 @@
 <template>
   <div id="app">
+    <codepen :options="codepenInput" />
   </div>
 </template>
 
 <script>
+import Codepen from './Codepen.vue';
 
 export default {
-  name: 'vue-codepen',
+  name: 'app',
+  components: {
+    Codepen,
+  },
+  data() {
+    return {
+      codepenInput: {
+        'data-height': '100%',
+        'data-theme-id': 'dark',
+        'data-slug-hash': 'JyxKMg',
+        'data-default-tab': 'result',
+        'data-user': 'sindael',
+        'data-embed-version': 2,
+        'data-pen-title': 'Fullscreen image gallery using Wallop, Greensock and Flexbox',
+        placeholder: `
+          See the Pen <a href="https://codepen.io/sindael/pen/JyxKMg/">here</a>.
+          `,
+        wrapperClass: 'codepen-embed',
+      },
+    };
+  },
 };
 </script>
-
 <style>
+body {
+  display: flex;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+
+#app {
+  margin: auto;
+  width: 75%;
+  height: 75%;
+}
+
+.codepen-embed {
+  height: 100%;
+}
 </style>
