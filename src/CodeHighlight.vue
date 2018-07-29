77d5dd5d24f89d9fdbab64dc04c51392ed09c0ed
Jan Bílek <honza@elisiondesign.cz>
2018-07-29 23:29:00 +0200

    Update styles and embed svgs

--- src/CodeHighlight.vue
+++ src/CodeHighlight.vue
@@ -29,15 +29,6 @@ export default {
   },
 };
 </script>
-<style scoped>
-  pre[class*="language-"] {
-    display: inline-block;
-    padding-right: 10em;
-    padding-top: 3rem;
-    border-radius: 6px;
-    background-image: url("./assets/window-controls.svg");
-    background-repeat: no-repeat;
-    background-position: 16px 16px;
-    box-shadow: 5px 5px 15px 0px rgba(50, 50, 50, 0.75);
-  }
+<style language="scss">
+@import '../themes/window.css';
 </style>
